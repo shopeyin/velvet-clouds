d85fd16e90d1f6e4f44975b9084f8d47c7f2fb5d
oladimeji <tommyslipz@gmail.com>
2023-07-03 23:00:33 +0100

    started the heaser

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import generateJoke from './generateJoke'
 import './styles/main.scss'
  import laughing from './assets/laughing.svg'
  
- import f1 from './assets/f1.jpeg'
+
 
 console.log('I was called again')
-// const laughImg = document.getElementById('laughImg')
-// laughImg.src = laughing
-
-const jokeBtn = document.getElementById('jokeBtn')
-jokeBtn.addEventListener('click', generateJoke)
-
-generateJoke()
--- webpack.config.js
+++ webpack.config.js
@@ -46,11 +46,15 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack App',
+      title: 'Velvet Cloud',
       filename: 'index.html',
        template: 'src/template.html',
     }),
